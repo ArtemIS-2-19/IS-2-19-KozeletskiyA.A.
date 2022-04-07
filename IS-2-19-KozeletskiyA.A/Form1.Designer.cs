4986a77dd08d495f2383fc8f7215d08a8e11dd11
ArtemIS-2-19 <94458574+ArtemIS-2-19@users.noreply.github.com>
2022-04-07 22:35:15 +0500

    Выполнил задание 1 и создал форму меню

---
+++ IS-2-19-KozeletskiyA.A/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿
+namespace IS_2_19_KozeletskiyA.A
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(5, 310);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(789, 196);
+            this.listBox1.TabIndex = 18;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LightGray;
+            this.label1.Location = new System.Drawing.Point(246, -68);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 26);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Заполните необходимые данные";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(331, 139);
+            this.textBox9.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(145, 22);
+            this.textBox9.TabIndex = 14;
+            this.textBox9.Text = "Артикул";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(466, 107);
+            this.textBox8.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(145, 22);
+            this.textBox8.TabIndex = 15;
+            this.textBox8.Text = "Объём Памяти";
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(466, 75);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(145, 22);
+            this.textBox7.TabIndex = 16;
+            this.textBox7.Text = "Производитель";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(466, 43);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(145, 22);
+            this.textBox6.TabIndex = 13;
+            this.textBox6.Text = "Частота GPU";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(198, 107);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(145, 22);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "Количество потоков";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(198, 75);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(145, 22);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "Количество ядер";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(198, 43);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(145, 22);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "Частота";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(331, 11);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(145, 22);
+            this.textBox2.TabIndex = 11;
+            this.textBox2.Text = "Год Выпуска";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(28, 13);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(145, 22);
+            this.textBox1.TabIndex = 12;
+            this.textBox1.Text = "Цена";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(466, 192);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 92);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Вывод информации о видеокарте";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 192);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 92);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Вывод информации о ЦП";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 538);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ IS-2-19-KozeletskiyA.A/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IS_2_19_KozeletskiyA.A
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Создание абстракного класса от которого будут наследоваться родительские
+        abstract class Комплектующие<art>
+        {
+            // переменные
+            protected int цена;
+            protected string год;
+            protected art арт;
+            // Конструктор
+            public Комплектующие(int цена1, string год1, art арт1)
+            {
+                цена = цена1;
+                год = год1;
+                арт = арт1;
+            }
+            // создание метода для вывода пользователей в лист бокс
+            public abstract void Display(ListBox lb);
+        }
+        // создание дочернего класса ЦП
+        class ЦП<art> : Комплектующие<art>
+        {
+            // переменные класса
+            double частота1;
+            int ядра1;
+            int потоки1;
+            // наследование от родительского класса Комплектующие
+            public ЦП(int цена1, string год1, art арт1, double поле1, int поле2, int поле3)
+                : base(цена1, год1, арт1)
+            {
+                частота = поле1;
+                ядра = поле2;
+                потоки = поле3;
+            }
+            // свойства для создания дополнительной логики полям
+            public double частота { get { return частота1; } set { частота1 = value; } }
+            public int ядра { get { return ядра1; } set { ядра1 = value; } }
+            public int потоки { get { return потоки1; } set { потоки1 = value; } }
+
+            // метод для вывода информации в лист бокс
+            public override void Display(ListBox listBox1)
+            {
+                listBox1.Items.Add($"Цена: {цена}, год Выпуска: {год}, арктикул: {арт}, частота: {частота}, количество ядер: {ядра}, количество потоков: {потоки}");
+            }
+        }
+        // создание дочернего класса Видеокарты
+        class Видеокарты<art> : Комплектующие<art>
+        {
+            double частота1;
+            string производитель1;
+            int память1;
+            // наследование от родительского класса Комплектующие
+            public Видеокарты(int цена1, string год1, art арт1, double поле1, string поле2, int поле3)
+                : base(цена1, год1, арт1)
+            {
+                частота = поле1;
+                производитель = поле2;
+                память = поле3;
+            }
+            // свойства для создания дополнительной логики полям
+            public double частота { get { return частота1; } set { частота1 = value; } }
+            public string производитель { get { return производитель1; } set { производитель1 = value; } }
+            public int память { get { return память1; } set { память1 = value; } }
+
+            // метод для вывода информации в лист бокс
+            public override void Display(ListBox listBox1)
+            {
+                listBox1.Items.Add($"Цена: {цена}, год Выпуска: {год}, арктикул: {арт}, частота GPU: {частота}, производитель: {производитель}, объём Памяти {память}");
+
+            }
+        }
+        // объявление экземпляра класса глобально
+        ЦП<string> цп;
+        Видеокарты<string> вк;
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            // присвоение переменным введенных данных в текст боксы
+            int цена1 = Convert.ToInt32(textBox1.Text);
+            string год1 = textBox2.Text;
+            double поле1 = Convert.ToDouble(textBox3.Text);
+            int поле2 = Convert.ToInt32(textBox4.Text);
+            int поле3 = Convert.ToInt32(textBox5.Text);
+            string арт1 = textBox9.Text;
+            // инициализания экземпляра класса и вызов ее конструктора
+            цп = new ЦП<string>(цена1, год1, арт1, поле1, поле2, поле3);
+            // вывод переменных в лист бокс с помощью метода
+            цп.Display(listBox1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // присвоение переменным введенных данных в текст боксы
+            int цена1 = Convert.ToInt32(textBox1.Text);
+            string год1 = textBox2.Text;
+            double поле1 = Convert.ToDouble(textBox6.Text);
+            string поле2 = textBox7.Text;
+            int поле3 = Convert.ToInt32(textBox8.Text);
+            string арт1 = textBox9.Text;
+            // инициализания экземпляра класса и вызов ее конструктора
+            вк = new Видеокарты<string>(цена1, год1, арт1, поле1, поле2, поле3);
+            // вывод переменных в лист бокс с мощью метода
+            вк.Display(listBox1);
+        }
+    }
+}
